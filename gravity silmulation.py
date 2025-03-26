cde299770fca77bfb65fb5edf9249545dcb2ee9a
이규민 <dlrbals719@gmail.com>
2025-03-26 13:18:38 +0900

    first motion

--- gravity silmulation.py
+++ gravity silmulation.py
@@ -1,22 +1,7 @@
 from vpython import *
 import controls
 
-x_axis = arrow(pos = vec(0, 0, 0), axis = vec(100, 0, 0), color = color.red, shaftwidth = 0.3)
-y_axis = arrow(pos = vec(0, 0, 0), axis = vec(0, 50, 0), color = color.green, shaftwidth = 0.3)
-
-theta = pi / 6
-direction = vec(cos(theta), sin(theta), 0)
-speed = 40
-velocity = speed * direction
-
-g = vec(0, -9.8, 0)
-
-stone = sphere(pos = vec(0, 10, 0), radius = 2, texture = textures.rock)
-
-wall = box(pos = vec(50, 0, 0), )
-
-dt = 0.01
-
+#global variable
 n_brick = 20
 brick_width = 2
 brick_height = 2
@@ -24,10 +9,35 @@ brick_depth = 1
 brick_x = 50
 brick_y = brick_height / 2
 boxes = []
+
+#shape
+x_axis = arrow(pos = vec(0, 0, 0), axis = vec(100, 0, 0), color = color.red, shaftwidth = 0.3)
+y_axis = arrow(pos = vec(0, 0, 0), axis = vec(0, 50, 0), color = color.green, shaftwidth = 0.3)
+stone = sphere(pos = vec(0, 10, 0), radius = 2, texture = textures.rock)
+wall = box(pos = vec(50, 0, 0), size = vec(2, 2, 1))
 for i in range(n_brick):
   box_temp = box(pos = vec(brick_x, brick_y, 0), size = vec(brick_width, brick_height, brick_depth), texture = textures.wood)
   boxes.append(box_temp)
   brick_y += brick_height
+
+
+#init parameter
+g = vec(0, -9.8, 0)
+theta = pi / 6
+direction = vec(cos(theta), sin(theta), 0)
+speed = 40
+velocity = speed * direction
+
+
+#time setting
+t = 0
+dt = 0.01
+t += dt
+
+
+
+dt = 0.01
+
 
 while(1):
   rate(100)
